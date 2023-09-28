32979c5679dea52da53eb37193e8bb64c383d04d
Aptemond <sofartem5@gmail.com>
2023-09-28 20:48:13 +0500

    add user auth

--- config/settings.py
+++ config/settings.py
@@ -136,8 +136,8 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sofartem5@gmail.com' #os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = 'Canavakill' #os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True
 
 # используется для тестирования
